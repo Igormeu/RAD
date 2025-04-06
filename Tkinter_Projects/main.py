b2af514f8151ab98c08ade5c6bd4efdd11723827
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-04-06 10:09:07 -0300

    Ajustei algumas estruturas do arquivo classes.py (#2)
    * Add files via upload
    
    * Update classes.py
    
    * reestruturação do projeto
    
    * saving progress
    
    * solve some errors

--- Tkinter_Projects/main.py
+++ Tkinter_Projects/main.py
@@ -1,4 +1,19 @@
 import tkinter as tk
 from tkinter import ttk 
 import sqlite3 as bd
-import classes as cls
+from classes import AppBd 
+
+db = AppBd()
+db.openConection()
+db.createTable()
+db.insertDate("maça",1.67)
+db.insertDate("Tomate",2.25)
+db.updateDate(id=1,name="batata", price=5.30)
+db.deleteDate(id=1)
+
+Dados = db.selectAllDate()
+
+print(Dados)
+
+db.closeConection()
+
