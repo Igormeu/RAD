e77b423ca77ea379b68c6e7f5b8f5a4ec85547bc
Igor Stenio <160430256+Igormeu@users.noreply.github.com>
2025-04-24 21:03:37 -0300

    Update main.py

--- Tkinter_Projects/main.py
+++ Tkinter_Projects/main.py
@@ -1,59 +1,108 @@
 import tkinter as tk
-from tkinter import ttk 
-import sqlite3 as bd
-import classes as cls
+from tkinter import ttk
+import modelo
+class PrincipalBD():
+    def __init__(self, win):
+       self.objbd = modelo.AppBd()
+       self.janela = win
 
+       self.treeProdutos = ttk.Treeview(self.janela,
+                                        columns=("Código do produto",
+                                           "Nome", 
+                                           "Preco"))
+       self.treeProdutos.heading("Código do produto", text= "Id: ")
+       self.treeProdutos.heading("Nome", text="Nome: ")
+       self.treeProdutos.heading("Preco", text="Preco: ")
+       self.exibirProdutos()
+       self.treeProdutos.pack() 
 
+       self.nome = tk.Label(self.janela, text="Nome: ")
+       self.nome.pack()
+       self.entrynome = tk.Entry(self.janela)
+       self.entrynome.pack()
+       self.preco = tk.Label(self.janela,
+                             text="Preco")
+       self.preco.pack()
+       self.entrypreco = tk.Entry(self.janela)
+       self.entrypreco.pack()
 
-#Integrar a janela aos modeulos criados
-class aplication():
-    def __init__(self, master=None):
-        self.modulo = cls.AppBd()
-        self.window = master
-        self.treeProduct = ttk.Treeview(self.window, columns=("Código", "Nome", "Preço"))
-        self.treeProduct.heading('Código',text="ID")
-        self.treeProduct.heading('Nome',text="Nome")
-        self.treeProduct.heading('Preço',text="Preço")
-        self.treeProduct.pack()
-        self.labelName = tk.Label(self.window, text="Nome: ")
-        self.labelName.pack()
-        self.entryName = tk.Entry(self.window)
-        self.entryName.pack()
-        self.labelPrice = tk.Label(self.window, text="Preço: ")
-        self.labelPrice.pack()
-        self.entryPrice = tk.Entry(self.window)
-        self.entryPrice.pack()
+       self.button = tk.Button(self.janela, text="Adicionar item", command=self.cadastrarProduto)
 
-    def registerProduct (self):
-        name = self.entryName.get()
-        price = self.entryPrice.get()
-        try:
-            price = float(price)
-        except:
-            price = 0
-        
-        self.modulo.insertDate(name,price)
-        
-        
-#criar janela princiopal
-window = tk.Tk()
+       self.button.pack()
+       
+       self.button_update = tk.Button(self.janela, text="Atualizar item", command=self.atualizarProdutos)
 
-root = aplication(window)
+       self.button_update.pack()
+       
+       self.button_delete = tk.Button(self.janela, text="Excluir item", command=self.excluirProdutos)
 
-#Customizar janela principal
-window.title("Janela principal")
-window.geometry("1200x900")
-window.mainloop()
+       self.button_delete.pack()
+    
+    def exibirProdutos (self):
+       try:
+          for item in self.treeProdutos.get_children():
+            self.treeProdutos.delete(item)
+            # or self.treeProdutos.delete(* self.treeProdutos.get_children())
+          products = self.objbd.select_all_products()
+          for product in products:
+            self.treeProdutos.insert("", tk.END, values=product) 
+       except:
+          print("Não deu para exibir")
+    def cadastrarProduto(self):
+      try: 
+         name = self.entrynome.get()
+         price = float(self.entrypreco.get())
+         self.objbd.inserirdados(name, price)
 
+         self.entrynome.delete(0,tk.END)
+         self.entrypreco.delete(0,tk.END)
+         self.exibirProdutos()
+         print ("Item cadastrado com sucesso !")
+      except Exception as e:
+         print ("Erro ao cadastrar o item")
+    def atualizarProdutos (self):
+       try:
+         selectedItem = self.treeProdutos.selection()
+         print(selectedItem)
+         item = self.treeProdutos.item(selectedItem)
+         print(item)
+         valores = item ['values']
+         print(valores) 
+         id = valores[0]
+         print(id)
+         name = self.entrynome.get()
+         print(name)
+         price = self.entrypreco.get()
+         print (price)
+         
+         name = valores[1] if len(name) == 0 else name
+         price = float(valores[2]) if price == "" else float(price)     
+             
+         self.objbd.update_product(id=id, name=name, price=price)
 
-
-#db = AppBd()
-#b.insertDate("maça",1.67)
-#db.insertDate("Tomate",2.25)
-#db.updateDate(id=1,name="batata", price=5.30)
-#db.deleteDate(id=1)
-
-#Dados = db.selectAllDate()
-
-#db.closeConection()
-
+         self.entrynome.delete(0,tk.END)
+         self.entrypreco.delete(0,tk.END)
+         
+         self.exibirProdutos()
+         
+       except Exception as e:
+          print(e)
+    def excluirProdutos (self):
+       try:
+         selectedItem = self.treeProdutos.selection()
+         item = self.treeProdutos.item(selectedItem)
+         valores = item ['values']
+         id = valores[0]
+             
+         self.objbd.delete_product(id)
+         
+         self.exibirProdutos()
+         
+       except Exception as e:
+          print(e)
+janela = tk.Tk() #criar a janela principal
+product_app = PrincipalBD(janela)
+#janela.title("Bem vindo ao sistema de cadastro!!")
+janela.geometry("900x700")
+janela.mainloop() 
+#apresentar janela, ate que o usuario feche
